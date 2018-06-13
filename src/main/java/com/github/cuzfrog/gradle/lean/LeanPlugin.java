6d4741139850c19033617a8b0a82b745326a1186
Cause Chung <cuzfrog@139.com>
2018-06-13 23:44:15 +0800

    Bootstrap class

---
+++ src/main/java/com/github/cuzfrog/gradle/lean/LeanPlugin.java
@@ -0,0 +1,11 @@
+package com.github.cuzfrog.gradle.lean;
+
+import org.gradle.api.Plugin;
+import org.gradle.api.Project;
+
+final class LeanPlugin implements Plugin<Project> {
+    @Override public void apply(final Project project) {
+        //TODO: not implemented
+        throw new AssertionError("Not implemented");
+    }
+}
---
+++ src/test/java/com/github/cuzfrog/gradle/lean/LeanPluginTest.java
@@ -0,0 +1,13 @@
+package com.github.cuzfrog.gradle.lean;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+final class LeanPluginTest {
+
+    @Test
+    void apply() {
+
+    }
+}
