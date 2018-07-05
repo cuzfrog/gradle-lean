7b0291b4236bea1025ced636cbd72aa06d67ca9b
Cause Chung <cuzfrog@139.com>
2018-07-05 21:56:33 +0800

    Implement DistZipLean

--- src/main/java/com/github/cuzfrog/gradle/lean/DistZipLean.java
+++ src/main/java/com/github/cuzfrog/gradle/lean/DistZipLean.java
@@ -1,16 +1,24 @@
 package com.github.cuzfrog.gradle.lean;
 
+import org.apache.commons.io.FilenameUtils;
 import org.gradle.api.DefaultTask;
 import org.gradle.api.plugins.ApplicationPlugin;
 import org.gradle.api.plugins.JavaPlugin;
 import org.gradle.api.tasks.TaskAction;
 import org.gradle.api.tasks.TaskContainer;
 import org.gradle.api.tasks.bundling.Zip;
+import org.gradle.internal.impldep.com.google.common.collect.ImmutableMap;
 import org.gradle.jvm.tasks.Jar;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.nio.file.FileSystem;
+import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.util.Map;
+import java.util.function.Consumer;
 
 class DistZipLean extends DefaultTask {
     private static final Logger logger = LoggerFactory.getLogger(DistZipLean.class);
@@ -30,7 +38,18 @@ class DistZipLean extends DefaultTask {
     }
 
     @TaskAction
-    void minimizeJars() throws Exception {
-        System.out.println(zipArchivePath);
+    void taskAction() throws IOException {
+        final Path minZipPath = genMinZipPath(zipArchivePath);
+        try {
+            Files.copy(zipArchivePath, minZipPath, StandardCopyOption.REPLACE_EXISTING);
+            ZipFsUtils.onZipFileSystem(minZipPath, zipFs -> JarUtils.minimizeJars(archivePath, zipFs.getPath("lib")));
+        }finally {
+            Files.deleteIfExists(minZipPath);
+        }
+    }
+
+    private static Path genMinZipPath(final Path zipArchivePath) {
+        final String name = FilenameUtils.getBaseName(zipArchivePath.toString()) + "-min.zip";
+        return zipArchivePath.resolveSibling(name);
     }
 }
