7a9c324e486d08c1eb54efcf4a8f58e3f4edcf6c
Cause Chung <cuzfrog@139.com>
2018-07-05 22:16:45 +0800

    Test against ZipFsUtils

--- src/main/java/com/github/cuzfrog/gradle/lean/ZipFsUtils.java
+++ src/main/java/com/github/cuzfrog/gradle/lean/ZipFsUtils.java
@@ -11,11 +11,17 @@ import java.util.Map;
 import java.util.function.Consumer;
 
 final class ZipFsUtils {
-    private static final Map<String, String> properties = ImmutableMap.of("create", "false");
+    private static final Map<String, String> noNewZipProps = ImmutableMap.of("create", "false");
+    private static final Map<String, String> newZipProps = ImmutableMap.of("create", "true");
 
     static void onZipFileSystem(final Path archivePath, final Consumer<FileSystem> action) throws IOException {
+        onZipFileSystem(archivePath, action, false);
+    }
+
+    static void onZipFileSystem(final Path archivePath, final Consumer<FileSystem> action, final boolean createNew) throws IOException {
         final URI jarUri = URI.create("jar:" + archivePath.toUri());
-        try (final FileSystem zipfs = FileSystems.newFileSystem(jarUri, properties)) {
+        final Map<String, String> zipProps = createNew ? newZipProps : noNewZipProps;
+        try (final FileSystem zipfs = FileSystems.newFileSystem(jarUri, zipProps)) {
             action.accept(zipfs);
         }
     }
---
+++ src/test/java/com/github/cuzfrog/gradle/lean/ZipFsUtilsTest.java
@@ -0,0 +1,37 @@
+package com.github.cuzfrog.gradle.lean;
+
+import org.gradle.internal.impldep.org.apache.commons.lang.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.assertj.core.api.Assertions.*;
+
+class ZipFsUtilsTest {
+
+    private static final Path tmpDir = TestFileSystem.createDir("/tmp");
+    private final Path zipPath = tmpDir.resolve("test.zip");
+
+    @Test
+    void onZipFileSystem() throws IOException {
+        final byte[] content = RandomStringUtils.randomAlphanumeric(200).getBytes();
+
+        ZipFsUtils.onZipFileSystem(zipPath, zipFs -> {
+            try {
+                Files.write(zipFs.getPath("file1"), content);
+            } catch (IOException e) {
+                fail("Create new file in new zip failed", e);
+            }
+        }, true);
+
+        ZipFsUtils.onZipFileSystem(zipPath, zipFs -> {
+            try {
+                assertThat(Files.readAllBytes(zipFs.getPath("file1"))).isEqualTo(content);
+            } catch (IOException e) {
+                fail("Read file in zip failed", e);
+            }
+        });
+    }
+}
