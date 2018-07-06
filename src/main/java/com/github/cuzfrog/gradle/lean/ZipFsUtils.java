ce0441df8671a6a1294a28322b93ef243516caa4
Cause Chung <cuzfrog@139.com>
2018-07-06 21:50:56 +0800

    Remove internal guava depencency

--- src/main/java/com/github/cuzfrog/gradle/lean/ZipFsUtils.java
+++ src/main/java/com/github/cuzfrog/gradle/lean/ZipFsUtils.java
@@ -1,18 +1,17 @@
 package com.github.cuzfrog.gradle.lean;
 
-import org.gradle.internal.impldep.com.google.common.collect.ImmutableMap;
-
 import java.io.IOException;
 import java.net.URI;
 import java.nio.file.FileSystem;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.function.Consumer;
 
 final class ZipFsUtils {
-    private static final Map<String, String> noNewZipProps = ImmutableMap.of("create", "false");
-    private static final Map<String, String> newZipProps = ImmutableMap.of("create", "true");
+    private static final Map<String, String> noNewZipProps = genProperties("create", "false");
+    private static final Map<String, String> newZipProps = genProperties("create", "true");
 
     static void onZipFileSystem(final Path archivePath, final Consumer<FileSystem> action) {
         onZipFileSystem(archivePath, action, false);
@@ -26,5 +25,11 @@ final class ZipFsUtils {
         }catch (final IOException e){
             throw new RuntimeException("Failed to open zip filesystem, archive path:" + archivePath, e);
         }
+    }
+
+    private static Map<String, String> genProperties(final String k, final String v){
+        return new HashMap<String, String>(){{
+            put(k, v);
+        }};
     }
 }
